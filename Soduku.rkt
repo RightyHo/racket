048abca990d5c48c738ac47b63ae369691c9ded7
Andrew Ho <andrewyc.ho@gmail.com>
2015-03-14 17:40:28 +0000

    Added new functions make-set() and list-possible()
    intention is to use these in the transform()  function

--- Soduku.rkt
+++ Soduku.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(struct square (grid row col possList) #:transparent)
+(struct square (grid row col poss-list) #:transparent)
 
 (define unsolved
   '((0 2 5 0 0 1 0 0 0)
@@ -13,3 +13,18 @@
     (0 0 0 0 7 8 1 0 3)
     (0 0 0 6 0 0 5 9 0)))
 
+(define possible (seteq 1 2 3 4 5 6 7 8 9))
+
+(define (make-set x)
+  (if (eq? x 0)
+      possible
+      (seteq x)))
+
+(define (list-possible unsolved)
+  (for/list ([x unsolved])
+    (if (list? x)
+        (map make-set x)
+        (printf "problem: ~a is not a list" x))))
+  
+
+      
