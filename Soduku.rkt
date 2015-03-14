b5c8c6b8ab47ec80249cdad35f6f38f70158560b
Andrew Ho <andrewyc.ho@gmail.com>
2015-03-14 21:44:34 +0000

    added more helper functions for the transform() function
    Included a list-index() function and also functions for defining
    which-grid() and for generating new-squares()

--- Soduku.rkt
+++ Soduku.rkt
@@ -25,6 +25,50 @@
     (if (list? x)
         (map make-set x)
         (printf "problem: ~a is not a list" x))))
-  
 
+;; Helper function to find out the index number of an element in a list.
+(define (list-index list element)
+  (let loop ((any-list list)
+             (index 0))
+    (cond ((empty? any-list) #f)
+          ((equal? (first any-list) element) index)
+          (else (loop (rest any-list) (add1 index))))))
+
+;; Sets grid id for the 9 3X3 grids on the matrix
+(define (which-grid r c)
+  (cond ((and (and (>= r 0) (< r 3)) 
+             (and (>= c 0) (< c 3))) 1)
+        ((and (and (>= r 0) (< r 3)) 
+             (and (>= c 3) (< c 6))) 2)
+        ((and (and (>= r 0) (< r 3)) 
+             (and (>= c 6) (< c 9))) 3)
+        ((and (and (>= r 3) (< r 6)) 
+             (and (>= c 0) (< c 3))) 4)
+        ((and (and (>= r 3) (< r 6)) 
+             (and (>= c 3) (< c 6))) 5)
+        ((and (and (>= r 3) (< r 6)) 
+             (and (>= c 6) (< c 9))) 6)
+        ((and (and (>= r 6) (< r 9)) 
+             (and (>= c 0) (< c 3))) 7)
+        ((and (and (>= r 6) (< r 9)) 
+             (and (>= c 3) (< c 6))) 8)
+        ((and (and (>= r 6) (< r 9)) 
+             (and (>= c 6) (< c 9))) 9)
+        (else 0)))
+   
+;; Takes a list of sets representing a single row on the matrix and returns a list of squares
+(define (new-squares poss-moves rowNum)
+  (let ((square-list empty))
+  (for ([i 9])
+    (let* ((move-set (list-ref poss-moves i))
+          (column i)
+          (grid (which-grid rowNum column)))
+      (drop (cons square-list (square grid rowNum column move-set)) 1)))))
+    
+;; takes a Sudoku puzzle in the above list of lists format, 
+;; and replaces each integer with a set of integers, thus returning a list of sets of integers.  
+(define (transform unsolved)
+  (let ((poss-set (list-possible unsolved)))
+    (map new-squares poss-set)))
+       
       
