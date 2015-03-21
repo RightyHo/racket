0a9847d5ddc57b0eadb6e8b0b93bbd453b0f39a7
Andrew Ho <andrewyc.ho@gmail.com>
2015-03-21 19:40:20 +0000

    wrote new function to reduce elements in a set

--- sudoku.rkt
+++ sudoku.rkt
@@ -1,5 +1,28 @@
 #lang racket
 
+   
+;; Takes a list of sets representing a single row of possible moves on the matrix and returns a list of squares
+;(define (new-squares poss-moves rowNum)
+;  (let ((square-list empty))
+;  (for ([i 9])
+;    (let* ((move-set (list-ref poss-moves i))
+;          (column i)
+;          (grid (which-grid rowNum column)))
+;      (drop (cons square-list (square grid rowNum column move-set)) 1)))))
+
+;; Splits up the possibility set matrix into rows to try and generate the new matrix of squares
+;(define (create-new-rows poss-sets)
+;  (let ((new-matrix empty))
+;  (for ([i 9])
+;      (let ((poss-row (list-ref poss-sets i)))
+;        (drop (cons new-matrix (new-squares poss-row i)) 1)))))
+
+;; returns true if the cell co-ordinates are for a singleton set
+;(define (is-singleton matrix row column)
+;  (if (= (set-count (car (take (drop matrix 0) 1))) 1)
+;      #t
+;      #f))
+
 ;; Example input list describing an initialised Sudoku board
 (define unsolved
   '((0 2 5 0 0 1 0 0 0)
@@ -11,6 +34,9 @@
     (0 9 1 5 0 0 6 0 0)
     (0 0 0 0 7 8 1 0 3)
     (0 0 0 6 0 0 5 9 0)))
+        
+;; The structure of each cell we will work with when trying to solve the puzzle
+(struct square (grid row col value) #:transparent)  
 
 ;; Set of all possible values
 (define possible (seteq 1 2 3 4 5 6 7 8 9))
@@ -49,22 +75,6 @@
     (if (list? x)
         (map make-set x)
         (printf "problem: ~a is not a list" x))))
-   
-;; Takes a list of sets representing a single row of possible moves on the matrix and returns a list of squares
-(define (new-squares poss-moves rowNum)
-  (let ((square-list empty))
-  (for ([i 9])
-    (let* ((move-set (list-ref poss-moves i))
-          (column i)
-          (grid (which-grid rowNum column)))
-      (drop (cons square-list (square grid rowNum column move-set)) 1)))))
-
-;; Splits up the possibility set matrix into rows to try and generate the new matrix of squares
-(define (create-new-rows poss-sets)
-  (let ((new-matrix empty))
-  (for ([i 9])
-      (let ((poss-row (list-ref poss-sets i)))
-        (drop (cons new-matrix (new-squares poss-row i)) 1)))))
 
 ;; takes a Sudoku puzzle in the above list of lists format, 
 ;; and replaces each integer with a set of integers, thus returning a list of sets of integers.  
@@ -74,10 +84,7 @@
 (define poss-matrix (transform unsolved))
 
 (define test-row (sixth poss-matrix))
-        
-;; The structure of each cell we will work with when trying to solve the puzzle
-(struct square (grid row col value) #:transparent)  
-  
+
 ;; finds all instances of x in the list and replaces them with y
 (define (replace lst x y)
  (cond
@@ -92,14 +99,6 @@
 (define (extract matrix row column sublist-size)
  (take (drop (car (take (drop matrix row)1))column)sublist-size))
 
-;; calls the replace method with the search key and replace values listed on the
-;; sub-list beginning at cell (row,column) and ending "sublist-size" cells to the right
-(define (manipulate matrix row column sublist-size)
- (let* ((initial-value (extract matrix row column sublist-size))
-       (possible-value (make-set initial-value)))
-   (replace (extract matrix row column sublist-size) (car initial-value) possible-value)))
-
-
 ;; where row and column parameters indicate the first cell to transform and
 ;; sublist-size indicates the number of adjacent cells you want to transform
 ;; the matrix rows and columns are numbered 0 to 8
@@ -112,7 +111,7 @@
           (flatten
            (list
             (take (first (take remainder 1)) column)  ; select the columns before the sublist (to remain unchanged) on the row to be manipulated
-            (manipulate matrix row column sublist-size) ; manipulate the values of the selected sublist
+            (remove-singleton-val matrix row column sublist-size) ; manipulate the values of the selected sublist
             (drop (first (take remainder 1)) (+ column sublist-size))))) ; select the remaining columns on the manipulated row after the sublist (to remain unchanged)         
           (drop matrix (+ row 1))))))
 
@@ -132,18 +131,11 @@
           (extract matrix 7 col 1)
           (extract matrix 8 col 1)))
 
-;; returns true if the cell co-ordinates are for a singleton set
-(define (is-singleton matrix row column)
-  (if (= (set-count (car (take (drop matrix 0) 1))) 1)
-      #t
-      #f))
-
 ;; returns true if set is a singleton
 (define (singleton cell)
   (if (= (set-count cell) 1)
       #t
       #f))
-
 
 ;; returns singleton elements in a list
 (define (singleton-list lst)
@@ -163,17 +155,33 @@
 
 ;; Find a location containing a singleton set (a set containing just one number).
 ;; For every other set in the same row, the same column, or the same 3x3 box, remove that number (if present).
-(define (singleton-search matrix)
-  (for/list ([i (in-naturals 1)]
-             [matrix])
-    (let* ((row-options (get-row matrix i))
-          (singletons-present (singleton-list row-options)))
+;(define (singleton-search matrix)
+;  (for/list ([i (in-naturals 1)]
+;             [matrix])
+;    (let* ((row-options (get-row matrix i))
+;          (singletons-present (singleton-list row-options))))))
+
+;; calls the replace method with the search key and replace values listed on the
+;; sub-list beginning at cell (row,column) and ending "sublist-size" cells to the right
+(define (remove-singleton-val matrix row column sublist-size init-val single)
+  (replace (extract matrix row column sublist-size) init-val (set-remove init-val single)))
       
-;; 
-(define 
-      (for/list ([j (in-naturals 1)]
-                 [singletons-present])
-        (let* ((val (singleton-valuecol-index (list-index row-options
+;; takes list of sets and reduces all sets by a particular value
+(define (reduce-choices matrix list row col acc-list single)
+  (cond
+    [(empty? list) acc-list]
+    [(set=? (car list) (seteq single)) (reduce-choices matrix (drop list 1) row (+ col 1) (append acc-list (car list)) single)]
+    [else (reduce-choices matrix (drop list 1) row (+ col 1) (append acc-list 
+                                                          (remove-singleton-val matrix row col 1 
+                                                                                (car list) single)) single)]))
+
+  
+  
+
+(define last-row (ninth poss-matrix))
+(define reduced-last-3 (reduce-choices poss-matrix last-row 8 0 '() 3))
+(define reduced-last-6 (reduce-choices poss-matrix last-row 8 0 '() 6))
+
                   
 
 (provide list-index
@@ -183,8 +191,6 @@
          square
          make-set
          list-possible
-         new-squares
-         create-new-rows
          transform)
          
     
